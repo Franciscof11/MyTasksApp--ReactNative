c01f5626b824d9657913a2d0140dd145ba9f44af
Francisco Gabriel <franciscogabrielf11@gmail.com>
2022-10-02 10:46:51 -0300

    Adicionando AsyncStorage

--- src/context/TasksContext.tsx
+++ src/context/TasksContext.tsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export interface Iprops {
     children: React.ReactElement;
@@ -9,6 +10,7 @@ export interface ITask {
     title: string;
 }
 
+const tasksData = "@MyTasks: Tasks";
 
 export interface ItaskContext {
     /*tipos do context*/
@@ -20,9 +22,17 @@ export const TasksContext = React.createContext<ItaskContext>({} as ItaskContext
 
 export const TasksProvider: React.FC<Iprops> = ({ children }) => {
 
+    const [data, setData] = useState<ITask[]>([]);
+
+    useEffect(() => {
+        async function loadTasks() {
+            const taskList = await AsyncStorage;
+        }
+    }, []);
+
     const addTask = (task: ITask) => {
         console.log("Add New Task Teste");
-            
+
     }
 
     const tasks = [{ id: '1', title: "hi" }]
